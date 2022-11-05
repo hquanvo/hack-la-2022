79a9e30d5a809475372a8fbf4ffa2f28816af92e
Norman Vo <vo.hoang.quan2003@gmail.com>
2022-11-05 10:00:31 -0700

    scatterplot for discussion added

--- src/get_started.py
+++ src/get_started.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 enroll = pd.read_csv('./data/additional/enrollments.csv', delimiter= ',')
-
 enroll[['user_id', 'type', 'last_activity_at', 'total_activity_time']]
 enroll[enroll.type != 'StudentViewEnrollment']
 
 discuss = pd.read_csv('./data/additional/discussions.csv')
 discuss = discuss.loc[:, discuss.columns != "post_id"]
-discuss_plot = discuss.plot.scatter(x = "discussion_topic_message_length", y = "post_message_length")
-discuss_plot
+plt.scatter(discuss.discussion_topic_message_length, discuss.post_message_length)
+plt.show()
